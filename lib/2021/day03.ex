721035091ae832f77675c2836177c7352cf74c1d
Sgiath <sgiath@sgiath.dev>
2021-12-03 10:47:14 +0100

    refactor: add few comments and common functions
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- lib/2021/day03.ex
+++ lib/2021/day03.ex
@@ -14,18 +14,31 @@ defmodule AdventOfCode.Year2021.Day03 do
 
   @impl AdventOfCode
   def part1(input) do
+    h = half(input)
+
     input
     |> Enum.zip()
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&Enum.sum/1)
-    |> Enum.map(&if(&1 > half(input), do: 1, else: 0))
-    |> Enum.reverse()
-    |> Enum.with_index()
-    |> Enum.reduce(0, &bor(&2, elem(&1, 0) <<< elem(&1, 1)))
+    |> Enum.map(&if(&1 > h, do: 1, else: 0))
+    |> list_to_integer()
     |> then(&(&1 * bxor(&1, 0b111111111111)))
   end
 
+  @doc """
+  Get half of the length of the list
+  """
   def half(input), do: div(length(input) + 1, 2)
+
+  @doc """
+  Convert list of 1 and 0 to integer
+  """
+  def list_to_integer(input) do
+    input
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.reduce(0, &bor(&2, elem(&1, 0) <<< elem(&1, 1)))
+  end
 
   @impl AdventOfCode
   def part2(input) do
@@ -34,12 +47,7 @@ defmodule AdventOfCode.Year2021.Day03 do
 
   def rating(input, filter, pos \\ 0)
 
-  def rating([result], _filter, _pos) do
-    result
-    |> Enum.reverse()
-    |> Enum.with_index()
-    |> Enum.reduce(0, &bor(&2, elem(&1, 0) <<< elem(&1, 1)))
-  end
+  def rating([result], _filter, _pos), do: list_to_integer(result)
 
   def rating(input, filter, pos) do
     f = freq(input, pos)
