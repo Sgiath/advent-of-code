7c716c5e83e6069842f38a728dcc7a81f34d55c3
Sgiath <sgiath@sgiath.dev>
2021-12-06 09:37:02 +0100

    feat: add auto-download of input data
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- lib/mix/tasks/init.ex
+++ lib/mix/tasks/init.ex
@@ -20,12 +20,20 @@ defmodule Mix.Tasks.AdventOfCode.Init do
     day = opts |> Keyword.get(:day) |> Integer.to_string() |> String.pad_leading(2, "0")
 
     template = Path.join([File.cwd!(), "priv", "template.eex"])
+    cookie = Path.join([File.cwd!(), "priv", "COOKIE"])
     script = Path.join([File.cwd!(), "lib", year, "day#{day}.ex"])
     input = Path.join([File.cwd!(), "priv", year, "day#{day}.in"])
 
     content = EEx.eval_file(template, year: year, day: day)
 
     File.open(script, [:write, :utf8], &IO.write(&1, content))
-    File.open(input, [:write, :utf8], &IO.write(&1, ""))
+
+    input_url = "http://adventofcode.com/#{year}/day/#{String.to_integer(day)}/input"
+    cookie = String.to_charlist("session=#{File.read!(cookie)}")
+
+    {:ok, {_status, _headers, input_data}} =
+      :httpc.request(:get, {input_url, [{'Cookie', cookie}]}, [], [])
+
+    File.open(input, [:write, :utf8], &IO.write(&1, input_data))
   end
 end
