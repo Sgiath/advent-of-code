37b3cd7d65b2b1c9445dd3e84ab8115a83d67e6c
Sgiath <sgiath@sgiath.dev>
2021-12-11 18:15:45 +0100

    refactor: convert list to charlist
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- lib/2021/day10.ex
+++ lib/2021/day10.ex
@@ -39,16 +39,14 @@ defmodule AdventOfCode.Year2021.Day10 do
   def part1(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.graphemes/1)
-    |> Enum.map(&check_syntax/1)
-    |> Enum.reduce(0, &score_syntax/2)
+    |> Enum.reduce(0, &(&1 |> check_syntax() |> score_syntax() |> Kernel.+(&2)))
   end
 
-  def score_syntax({:error, ")"}, acc), do: acc + 3
-  def score_syntax({:error, "]"}, acc), do: acc + 57
-  def score_syntax({:error, "}"}, acc), do: acc + 1197
-  def score_syntax({:error, ">"}, acc), do: acc + 25137
-  def score_syntax({:incomplete, _completion}, acc), do: acc
+  def score_syntax({:error, ?)}), do: 3
+  def score_syntax({:error, ?]}), do: 57
+  def score_syntax({:error, ?}}), do: 1197
+  def score_syntax({:error, ?>}), do: 25137
+  def score_syntax({:incomplete, _completion}), do: 0
 
   # ===============================================================================================
   # Part 2
@@ -58,10 +56,8 @@ defmodule AdventOfCode.Year2021.Day10 do
   def part2(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.graphemes/1)
-    |> Enum.map(&check_syntax/1)
-    |> Enum.filter(&(elem(&1, 0) == :incomplete))
-    |> Enum.map(&score_completion/1)
+    |> Enum.map(&(&1 |> check_syntax() |> score_completion()))
+    |> Enum.reject(&is_nil/1)
     |> Statistics.median()
   end
 
@@ -69,11 +65,12 @@ defmodule AdventOfCode.Year2021.Day10 do
   Score completion based on the received stack
   """
   def score_completion(result, acc \\ 0)
+  def score_completion({:error, _char}, 0), do: nil
   def score_completion({:incomplete, stack}, 0), do: score_completion(stack, 0)
-  def score_completion(["(" | stack], acc), do: score_completion(stack, acc * 5 + 1)
-  def score_completion(["[" | stack], acc), do: score_completion(stack, acc * 5 + 2)
-  def score_completion(["{" | stack], acc), do: score_completion(stack, acc * 5 + 3)
-  def score_completion(["<" | stack], acc), do: score_completion(stack, acc * 5 + 4)
+  def score_completion([?( | stack], acc), do: score_completion(stack, acc * 5 + 1)
+  def score_completion([?[ | stack], acc), do: score_completion(stack, acc * 5 + 2)
+  def score_completion([?{ | stack], acc), do: score_completion(stack, acc * 5 + 3)
+  def score_completion([?< | stack], acc), do: score_completion(stack, acc * 5 + 4)
   def score_completion([], acc), do: acc
 
   # ===============================================================================================
@@ -83,27 +80,27 @@ defmodule AdventOfCode.Year2021.Day10 do
   @doc """
   Check if bracket is starting one
   """
-  defguard is_starting(a) when a in ["(", "[", "{", "<"]
+  defguard is_starting(a) when a in '([{<'
 
   @doc """
   Check if two brackets are simple bracket pair
   """
-  defguard is_simple(a, b) when a == "(" and b == ")"
+  defguard is_simple(a, b) when a == ?( and b == ?)
 
   @doc """
   Check if two brackets are square bracket pair
   """
-  defguard is_square(a, b) when a == "[" and b == "]"
+  defguard is_square(a, b) when a == ?[ and b == ?]
 
   @doc """
   Check if two brackets are curly bracket pair
   """
-  defguard is_curly(a, b) when a == "{" and b == "}"
+  defguard is_curly(a, b) when a == ?{ and b == ?}
 
   @doc """
   Check if two brackets are pointy bracket pair
   """
-  defguard is_pointy(a, b) when a == "<" and b == ">"
+  defguard is_pointy(a, b) when a == ?< and b == ?>
 
   @doc """
   Check if two brackets are any bracket pair
@@ -121,13 +118,17 @@ defmodule AdventOfCode.Year2021.Day10 do
   """
   def check_syntax(line, stack \\ [])
   # add starting bracket to stack
-  def check_syntax([a | rest], stack) when is_starting(a), do: check_syntax(rest, [a | stack])
+  def check_syntax(<<a::8, rest::binary>>, stack) when is_starting(a),
+    do: check_syntax(rest, [a | stack])
+
   # remove bracket from stack on complete pair
-  def check_syntax([a | rest], [b | stack]) when is_pair(b, a), do: check_syntax(rest, stack)
+  def check_syntax(<<a::8, rest::binary>>, [b | stack]) when is_pair(b, a),
+    do: check_syntax(rest, stack)
+
   # return error when bracket is not in pair with bracket on stack
-  def check_syntax([a | _rest], _stack), do: {:error, a}
+  def check_syntax(<<a::8, _rest::binary>>, _stack), do: {:error, a}
   # return incomplete when we are at the end but stack is not empty
-  def check_syntax([], stack) when length(stack) > 0, do: {:incomplete, stack}
+  def check_syntax(<<>>, stack) when length(stack) > 0, do: {:incomplete, stack}
   # return success when we are at the end of line and stack is empty
-  def check_syntax([], []), do: :success
+  def check_syntax(<<>>, []), do: :success
 end
