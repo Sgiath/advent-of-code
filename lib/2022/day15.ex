01f9f9ccd60f7931dfd78b9359082fa9c3325025
Sgiath <sgiath@sgiath.dev>
2022-12-15 18:45:03 +0100

    feat: year 2022, day 15, part 1
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

---
+++ lib/2022/day15.ex
@@ -0,0 +1,96 @@
+defmodule AdventOfCode.Year2022.Day15 do
+  @moduledoc ~S"""
+  https://adventofcode.com/2022/day/15
+  """
+  use AdventOfCode, year: 2022, day: 15
+
+  # =============================================================================================
+  # Input
+  # =============================================================================================
+
+  @impl AdventOfCode
+  def test_input do
+    """
+    Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    Sensor at x=9, y=16: closest beacon is at x=10, y=16
+    Sensor at x=13, y=2: closest beacon is at x=15, y=3
+    Sensor at x=12, y=14: closest beacon is at x=10, y=16
+    Sensor at x=10, y=20: closest beacon is at x=10, y=16
+    Sensor at x=14, y=17: closest beacon is at x=10, y=16
+    Sensor at x=8, y=7: closest beacon is at x=2, y=10
+    Sensor at x=2, y=0: closest beacon is at x=2, y=10
+    Sensor at x=0, y=11: closest beacon is at x=2, y=10
+    Sensor at x=20, y=14: closest beacon is at x=25, y=17
+    Sensor at x=17, y=20: closest beacon is at x=21, y=22
+    Sensor at x=16, y=7: closest beacon is at x=15, y=3
+    Sensor at x=14, y=3: closest beacon is at x=15, y=3
+    Sensor at x=20, y=1: closest beacon is at x=15, y=3
+    """
+  end
+
+  def parse(input) do
+    input
+    |> String.split(["\n"], trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line("Sensor at x=" <> line) do
+    %{"xb" => xb, "xs" => xs, "yb" => yb, "ys" => ys} =
+      Regex.named_captures(
+        ~r/(?<xs>-?\d+), y=(?<ys>-?\d+): closest beacon is at x=(?<xb>-?\d+), y=(?<yb>-?\d+)/,
+        line
+      )
+
+    {{String.to_integer(xs), String.to_integer(ys)},
+     {String.to_integer(xb), String.to_integer(yb)}}
+  end
+
+  # =============================================================================================
+  # Part 1
+  # =============================================================================================
+  defguard md(x1, y1, x2, y2, d) when abs(x1 - x2) + abs(y1 - y2) <= d
+
+  @impl AdventOfCode
+  def part1(input, row \\ 2_000_000) do
+    sensors = parse(input)
+
+    b =
+      sensors
+      |> Enum.uniq_by(fn {_s, b} -> b end)
+      |> Enum.count(fn {_s, {_x, y}} -> y == row end)
+
+    s =
+      sensors
+      |> Enum.reduce([], fn {{xs, ys}, {xb, yb}}, map ->
+        d = distance({xs, ys}, {xb, yb})
+
+        if row in (ys - d)..(ys + d) do
+          [(xs - d + abs(ys - row))..(xs + d - abs(ys - row)) | map]
+        else
+          map
+        end
+      end)
+      |> Enum.map(&Enum.into(&1, MapSet.new()))
+      |> Enum.reduce(&MapSet.union/2)
+      |> MapSet.size()
+
+    s - b
+  end
+
+  def distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
+
+  # =============================================================================================
+  # Part 2
+  # =============================================================================================
+
+  @impl AdventOfCode
+  def part2(input) do
+    input
+    |> parse()
+    |> dbg()
+  end
+
+  # =============================================================================================
+  # Utils
+  # =============================================================================================
+end
---
+++ test/2022/day15_test.exs
@@ -0,0 +1,18 @@
+defmodule AdventOfCode.Year2022.Day15.Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode.Year2022.Day15
+
+  doctest AdventOfCode.Year2022.Day15
+
+  test "part 1" do
+    assert Day15.part1(Day15.test_input()) == 0
+    assert Day15.part1(Day15.input()) == 0
+  end
+
+  @tag skip: "not implemented"
+  test "part 2" do
+    assert Day15.part2(Day15.test_input()) == 0
+    assert Day15.part2(Day15.input()) == 0
+  end
+end
