b98987ef0c2b5a01fa74a8e519dfb4d96c562936
Sgiath <sgiath@sgiath.dev>
2024-09-23 08:25:15 +0000

    feat: add Nix deps
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "CrazyEgg Auth V2";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ parts, ... }:
+    parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { self', pkgs, ... }:
+        let
+          beamPackages = pkgs.beam_minimal.packages.erlang_27;
+          elixir = beamPackages.elixir_1_17;
+        in
+        {
+          devShells = {
+            default = self'.devShells.develop;
+            develop = pkgs.mkShell {
+              packages = [
+                elixir
+                pkgs.zig
+              ];
+              env = {
+                ERL_AFLAGS = "+pc unicode -kernel shell_history enabled";
+                ELIXIR_ERL_OPTIONS = "+fnu +sssdio 128";
+              };
+            };
+          };
+        };
+    };
+}
